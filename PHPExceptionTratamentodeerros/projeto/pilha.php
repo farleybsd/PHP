2e8b12c2ec44d8a4d48579cad59cd84d7428b630
farleybsd <farley.t.i@hotmail.com>
2020-05-18 10:24:40 -0300

    Pegando na Pilha

--- PHPExceptionTratamentodeerros/projeto/pilha.php
+++ PHPExceptionTratamentodeerros/projeto/pilha.php
@@ -4,23 +4,28 @@ function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
 
-    try {
-        $arrayfixo = new SplFixedArray(2);
-        $arrayfixo[3] = 'Valor';
-    } catch (RuntimeException $problema) {
-        echo "Aconteceu um erro na função 1" . PHP_EOL;
-    }
-
-
-
+    // try {
+    //     $arrayfixo = new SplFixedArray(2);
+    //     $arrayfixo[3] = 'Valor';
+    // } catch (RuntimeException $problema) {
+    //     echo "Aconteceu um erro na função 1" . PHP_EOL;
+    // }
     //$divisao = intdiv(5, 0);
-    funcao2();
+    try {
+        funcao2();
+    } catch (RuntimeException $problema) {
+        echo "Na funcao 1 resolvi o problema na funcao Dois" . PHP_EOL;
+    }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $arrayfixo = new SplFixedArray(2);
+    $arrayfixo[3] = 'Valor';
+    $divisao = intdiv(5, 0);
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
