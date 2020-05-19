429623c07a2821c58acc9116272650ef5d8d79be
farleybsd <farley.t.i@hotmail.com>
2020-05-19 17:44:49 -0300

    Extraindo classe

--- Composer/Buscador-Cursos-Alura/Buscar-Cursos.php
+++ Composer/Buscador-Cursos-Alura/Buscar-Cursos.php
@@ -1,23 +1,23 @@
 <?php
 
-require 'vendor/autoload.php';
+use GuzzleHttp\Psr7\Request;
 
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new \GuzzleHttp\Client([
+$client = new Client([
     'base_uri' => 'https://www.alura.com.br/',
     'verify' => false
 ]);
-$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
-
-$html = $resposta->getBody();
-
 $crawler = new Crawler();
-$crawler->addHtmlContent($html);
 
-$cursos = $crawler->filter('span.card-curso__nome');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo $curso->textContent . PHP_EOL;
+    echo $curso . PHP_EOL;
 }
---
+++ Composer/Buscador-Cursos-Alura/src/Buscador.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+
+    private $httpClient;
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        $resposta = $this->httpClient->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+        $html = $resposta->getBody();
+
+        $crawler = new Crawler();
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+}
